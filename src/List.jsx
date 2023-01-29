38a5bbd4f3d9e8b90ab8d304b62a4d20351f1e8f
Sachin Ronson Dsilva <110468373+sachindsilva16@users.noreply.github.com>
2023-01-29 17:46:28 +0530

    Initial commit

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Noodles</li>
+      <li>Pizza</li>
+    </ul>
+  );
+}
+
+export default List;
